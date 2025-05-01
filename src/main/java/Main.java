5f3bd2568fee0bbc9c3398a09e38c839ff4ee14b
荆智彬 <2537964099@qq.com>
2025-05-01 22:13:29 +0800

    上传

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,40 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 
 import java.util.List;
 public class Main {
     public static void main(String[] args) throws Exception{
+        //如果你的edge不是135.0.3179.98正式版，将edge更新至此版本
+        //或自行下载对应版本的EdgeWebDriver替换msedgedriver.exe文件
         EdgeDriver driver;
         System.setProperty("webdriver.edge.driver", "src/main/resources/msedgedriver.exe");
         driver = new EdgeDriver();
+
+//      如果使用的是Chrome，改为使用这三行
+//      并自行下载与你版本对应的ChromeWebDriver放到resources目录，同时完善下面的路径
+//        ChromeDriver driver;
+//        System.setProperty("webdriver.edge.driver", "src/main/resources/");
+//        driver = new ChromeDriver();
+
         driver.manage().window().maximize();
         driver.get("https://passport2.chaoxing.com/login?");
 
         WebElement ph = driver.findElement(By.xpath("//*[@id=\"phone\"]"));
         //填入超星账号，例如ph.sendKeys("123123123");
-        ph.sendKeys("18947617481");
+        ph.sendKeys("");
 
         WebElement pa = driver.findElement(By.xpath("//*[@id=\"pwd\"]"));
         //填入超星密码，例如pa.sendKeys("123123123");
-        pa.sendKeys("aw/\\154'q");
+        pa.sendKeys("");
         WebElement logbt = driver.findElement(By.xpath("//*[@id=\"loginBtn\"]"));
         logbt.click();
         Thread.sleep(1000);
 
         //填入想要刷的课程的knowledge-all界面网址
         //形如driver.get("https://tsjy.chaoxing.com/plaza/knowledge-all?...")
-        driver.get("https://tsjy.chaoxing.com/plaza/knowledge-all?classifyId=987539191&courseId=252557203&personId=273799278&classId=121303602&userId=239760581");
-//        driver.get("");
+        driver.get("");
         Thread.sleep(1000);
         driver.findElement(By.partialLinkText("全部")).click();
         Thread.sleep(1000);
